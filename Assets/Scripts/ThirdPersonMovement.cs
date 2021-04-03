4f81a10d24025ecd469e34d53e6d9111fdb75875
Ramog <ramon.fischer1@gmx.de>
2021-04-03 14:48:27 +0200

    Enabled FXAA on Camara, Fixed Dependency of Movement Controller on Player
    Since it has barely any performance impact and I just can't look at stuff without AA
    Also editet the ThirdPersonMovement Script, made it so the Character Controller is a requirement instead of a public variable.

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class ThirdPersonMovement : MonoBehaviour
 //Code by Brackeys (https://www.youtube.com/watch?v=4HpC--2iowE) 
 {
-    public CharacterController controller;
+
+    private CharacterController controller;
+
     public Transform cam;
 
     public float joystickDeadzone = 0f;
@@ -17,13 +20,15 @@ public class ThirdPersonMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Since the Controller is a Component that is on the Player I would make it a required component -> less room for error
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")).normalized;
+
         if (direction.magnitude > joystickDeadzone)
         {
             float targAtangle = Mathf.Atan2(direction.x,direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
