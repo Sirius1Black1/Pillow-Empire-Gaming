d75e46a5aa935935231b5eca113f28c0d7e63e60
JT-codedump <58177844+JT-codedump@users.noreply.github.com>
2021-04-03 09:05:16 +0200

    Movement implementation, switch to Cinemachine for camerra control

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(GameObject))]
+public class Movement : MonoBehaviour
+{
+
+    [SerializeField]
+    Camera cam = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (cam == null)
+            cam = (Camera)gameObject.GetComponentInChildren(typeof(Camera),true);
+        if (cam == null)
+            Debug.LogError("Camera not found!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+}
---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+//Code by Brackeys (https://www.youtube.com/watch?v=4HpC--2iowE) 
+{
+    public CharacterController controller;
+    public Transform cam;
+
+    public float joystickDeadzone = 0f;
+
+    public float speed = 5f;
+    public float turnSpeed = 0.1f;
+    float turnSmoothVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")).normalized;
+        if (direction.magnitude > joystickDeadzone)
+        {
+            float targAtangle = Mathf.Atan2(direction.x,direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targAtangle, ref turnSmoothVelocity, turnSpeed);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targAtangle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+    }
+}
