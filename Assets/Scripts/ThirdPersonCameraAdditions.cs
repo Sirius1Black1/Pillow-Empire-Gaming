b89f0b45c3d625b9938796bee98ed7ddf03813b5
JT-codedump <58177844+JT-codedump@users.noreply.github.com>
2021-04-05 19:27:23 +0200

    minor fix in camera

---
+++ Assets/Scripts/ThirdPersonCameraAdditions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraAdditions : MonoBehaviour
+{
+    public Cinemachine.CinemachineFreeLook mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //mainCamera.m_Orbits[0].m_Radius = 20;
+
+    }
+}
--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
 public class ThirdPersonMovement : MonoBehaviour
-//Code by Brackeys (https://www.youtube.com/watch?v=4HpC--2iowE) 
+//Code based on Brackeys (https://www.youtube.com/watch?v=4HpC--2iowE) 
 {
 
     private CharacterController controller;
 
     public Transform cam;
 
-    public float joystickDeadzone = 0f;
-
     public float speed = 5f;
+    public float runSpeed = 8f;
+    public float sneakSpeed = 3f;
+
     public float turnSpeed = 0.1f;
     float turnSmoothVelocity;
 
@@ -28,15 +29,30 @@ public class ThirdPersonMovement : MonoBehaviour
     void Update()
     {
         Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")).normalized;
+<<<<<<< Updated upstream
 
         if (direction.magnitude > joystickDeadzone)
+=======
+        float targAtangle = Mathf.Atan2(direction.x,direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targAtangle, ref turnSmoothVelocity, turnSpeed);
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        Vector3 moveDir = Quaternion.Euler(0f, targAtangle, 0f) * Vector3.forward;
+            
+        if (Input.GetKey(KeyCode.LeftShift))
+>>>>>>> Stashed changes
         {
-            float targAtangle = Mathf.Atan2(direction.x,direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targAtangle, ref turnSmoothVelocity, turnSpeed);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            Vector3 moveDir = Quaternion.Euler(0f, targAtangle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            controller.Move(moveDir.normalized * runSpeed * Time.deltaTime * direction.magnitude);
         }
+        else if (Input.GetKey(KeyCode.LeftControl))
+        {
+            controller.Move(moveDir.normalized * sneakSpeed * Time.deltaTime * direction.magnitude);
+        }
+        else
+        {
+            controller.Move(moveDir.normalized * speed * Time.deltaTime * direction.magnitude);
+        }
+        
+
+
     }
 }
