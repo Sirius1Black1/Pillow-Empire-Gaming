89d5f63f580977820fb850b6a365a15f1c4b158f
Ramog <ramon.fischer1@gmx.de>
2021-05-07 19:00:18 +0200

    added network menu

---
+++ Assets/NetworkSettings.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAPI;
+
+namespace HelloWorld
+{
+    public class NetworkSettings : MonoBehaviour
+    {
+        public void ButtonHostPress()
+        {
+            if (NetworkManager.Singleton.IsServer)
+            {
+                NetworkManager.Singleton.StopServer();
+            }
+
+            if (NetworkManager.Singleton.IsClient)
+            {
+                NetworkManager.Singleton.StopClient();
+            }
+
+            if (!NetworkManager.Singleton.IsHost)
+            {
+                NetworkManager.Singleton.StartHost();
+            }
+        }
+
+        public void ButtonClientPress()
+        {
+            if (NetworkManager.Singleton.IsServer)
+            {
+                NetworkManager.Singleton.StopServer();
+            }
+            
+            if (NetworkManager.Singleton.IsHost)
+            {
+                NetworkManager.Singleton.StopHost();
+            }
+
+            if (!NetworkManager.Singleton.IsClient)
+            {
+                NetworkManager.Singleton.StartClient();
+            }
+        }
+
+        public void ButtonServerPress()
+        {
+            if (NetworkManager.Singleton.IsHost)
+            {
+                NetworkManager.Singleton.StopHost();
+            }
+            
+            if (NetworkManager.Singleton.IsClient)
+            {
+                NetworkManager.Singleton.StopClient();
+            }
+
+            if (!NetworkManager.Singleton.IsServer)
+            {
+                NetworkManager.Singleton.StartServer();
+            }
+        }
+    }
+}
